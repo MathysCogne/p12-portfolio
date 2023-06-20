506880a67eef3b3dff2daaf335f222c59098ae8e
Mathys Cogné-Foucault <124098493+MathysCogne@users.noreply.github.com>
2023-06-20 23:20:33 +0200

    Add files via upload

---
+++ js/blurNav.js
@@ -0,0 +1,24 @@
+const navItems = document.querySelectorAll('.nav-item');
+let timeoutId;
+
+navItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    clearTimeout(timeoutId);
+    navItems.forEach(otherItem => {
+      otherItem.classList.remove('flou');
+    });
+    navItems.forEach(otherItem => {
+      if (otherItem !== item) {
+        otherItem.classList.add('flou');
+      }
+    });
+  });
+
+  item.addEventListener('mouseleave', () => {
+    timeoutId = setTimeout(() => {
+      navItems.forEach(otherItem => {
+        otherItem.classList.remove('flou');
+      });
+    }, 200);
+  });
+});
---
+++ js/cursor.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var cursor = document.getElementById('custom-cursor');
+   var cursorSize = cursor.offsetWidth;
+   var mouseX = 0;
+   var mouseY = 0;
+   var posX = 0;
+   var posY = 0;
+   var easing = 0.14;
+ 
+   function updateCursor() {
+     var distX = (mouseX - cursorSize / 2 - posX) * easing;
+     var distY = (mouseY - cursorSize / 2 - posY) * easing;
+     posX += distX;
+     posY += distY;
+ 
+     cursor.style.left = posX + 'px';
+     cursor.style.top = posY + 'px';
+ 
+     requestAnimationFrame(updateCursor);
+   }
+ 
+   document.addEventListener('mousemove', function(e) {
+     mouseX = e.clientX;
+     mouseY = e.clientY;
+   });
+ 
+   updateCursor();
+ });
+ 
