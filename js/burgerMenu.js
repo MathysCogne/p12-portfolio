ded6779f06c01852b9bee1f23fabf1942b627157
Mathys Cogné <mathys.cognef@gmail.com>
2023-06-27 22:10:58 +0200

    Fix Burger menu and view on mobile

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -7,3 +7,11 @@ document.getElementById("close-button").addEventListener("click", function() {
    var menu = document.getElementById("burger-menu");
    menu.style.display = "none";
 });
+
+var menuButtons = document.getElementsByClassName("nav-item");
+for (var i = 0; i < menuButtons.length; i++) {
+   menuButtons[i].addEventListener("click", function() {
+      var menu = document.getElementById("burger-menu");
+      menu.style.display = "none";
+   });
+}
